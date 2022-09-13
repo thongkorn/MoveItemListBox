87b319435fd6ed6443408acbd0046b5d82f716ab
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2022-09-13 13:32:40 +0700

    Add files via upload

---
+++ MoveItemListBox/MoveItemListBox/frmMoveItemListBox.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMoveItemListBox
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.btnMoveRight = New System.Windows.Forms.Button()
+        Me.btnMoveLeft = New System.Windows.Forms.Button()
+        Me.btnMoveAllRight = New System.Windows.Forms.Button()
+        Me.btnMoveAllLeft = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnMoveUp = New System.Windows.Forms.Button()
+        Me.btnMoveDown = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(24, 40)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.ListBox1.Size = New System.Drawing.Size(183, 356)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(273, 40)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.ListBox2.Size = New System.Drawing.Size(183, 356)
+        Me.ListBox2.TabIndex = 5
+        '
+        'btnMoveRight
+        '
+        Me.btnMoveRight.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMoveRight.Location = New System.Drawing.Point(213, 55)
+        Me.btnMoveRight.Name = "btnMoveRight"
+        Me.btnMoveRight.Size = New System.Drawing.Size(54, 26)
+        Me.btnMoveRight.TabIndex = 1
+        Me.btnMoveRight.Text = ">"
+        Me.btnMoveRight.UseVisualStyleBackColor = True
+        '
+        'btnMoveLeft
+        '
+        Me.btnMoveLeft.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMoveLeft.Location = New System.Drawing.Point(213, 88)
+        Me.btnMoveLeft.Name = "btnMoveLeft"
+        Me.btnMoveLeft.Size = New System.Drawing.Size(54, 26)
+        Me.btnMoveLeft.TabIndex = 2
+        Me.btnMoveLeft.Text = "<"
+        Me.btnMoveLeft.UseVisualStyleBackColor = True
+        '
+        'btnMoveAllRight
+        '
+        Me.btnMoveAllRight.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMoveAllRight.Location = New System.Drawing.Point(213, 121)
+        Me.btnMoveAllRight.Name = "btnMoveAllRight"
+        Me.btnMoveAllRight.Size = New System.Drawing.Size(54, 26)
+        Me.btnMoveAllRight.TabIndex = 3
+        Me.btnMoveAllRight.Text = ">>"
+        Me.btnMoveAllRight.UseVisualStyleBackColor = True
+        '
+        'btnMoveAllLeft
+        '
+        Me.btnMoveAllLeft.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMoveAllLeft.Location = New System.Drawing.Point(213, 154)
+        Me.btnMoveAllLeft.Name = "btnMoveAllLeft"
+        Me.btnMoveAllLeft.Size = New System.Drawing.Size(54, 26)
+        Me.btnMoveAllLeft.TabIndex = 4
+        Me.btnMoveAllLeft.Text = "<<"
+        Me.btnMoveAllLeft.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 16)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "รายการที่ 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(270, 21)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 16)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "รายการที่ 2"
+        '
+        'btnMoveUp
+        '
+        Me.btnMoveUp.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMoveUp.Location = New System.Drawing.Point(462, 55)
+        Me.btnMoveUp.Name = "btnMoveUp"
+        Me.btnMoveUp.Size = New System.Drawing.Size(54, 26)
+        Me.btnMoveUp.TabIndex = 6
+        Me.btnMoveUp.Text = "UP"
+        Me.btnMoveUp.UseVisualStyleBackColor = True
+        '
+        'btnMoveDown
+        '
+        Me.btnMoveDown.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMoveDown.Location = New System.Drawing.Point(462, 88)
+        Me.btnMoveDown.Name = "btnMoveDown"
+        Me.btnMoveDown.Size = New System.Drawing.Size(54, 26)
+        Me.btnMoveDown.TabIndex = 7
+        Me.btnMoveDown.Text = "DOWN"
+        Me.btnMoveDown.UseVisualStyleBackColor = True
+        '
+        'frmMoveItemListBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(520, 416)
+        Me.Controls.Add(Me.btnMoveDown)
+        Me.Controls.Add(Me.btnMoveUp)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnMoveAllLeft)
+        Me.Controls.Add(Me.btnMoveAllRight)
+        Me.Controls.Add(Me.btnMoveLeft)
+        Me.Controls.Add(Me.btnMoveRight)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMoveItemListBox"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Move Item from ListBox to ListBox - coDe bY: Thongkorn Tubtimkrob"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents btnMoveRight As System.Windows.Forms.Button
+    Friend WithEvents btnMoveLeft As System.Windows.Forms.Button
+    Friend WithEvents btnMoveAllRight As System.Windows.Forms.Button
+    Friend WithEvents btnMoveAllLeft As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnMoveUp As System.Windows.Forms.Button
+    Friend WithEvents btnMoveDown As System.Windows.Forms.Button
+
+End Class
---
+++ MoveItemListBox/MoveItemListBox/frmMoveItemListBox.vb
@@ -0,0 +1,153 @@
+﻿#Region "ABOUT"
+' / ------------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gsoft.com
+' /
+' / Purpose: Transfer Items Between 2 ListBoxes and Move Up/Down Item.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / ------------------------------------------------------------------------------
+#End Region
+
+Public Class frmMoveItemListBox
+
+    Private Sub frmMoveItemListBox_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        '// ใส่ค่าไอเทมตัวอย่าง
+        With ListBox1
+            .Items.Add("Computer")
+            .Items.Add("Processor")
+            .Items.Add("Notebook")
+            .Items.Add("Mouse")
+            .Items.Add("Keyboard")
+            .Items.Add("Power Supply")
+            .Items.Add("Monitor")
+        End With
+        '// เลือกไอเทมได้ทีละตัว
+        'ListBox1.SelectionMode = SelectionMode.One
+        'ListBox2.SelectionMode = SelectionMode.One
+        '// เพิ่มคำอธิบาย (ToolTip) บนปุ่มคำสั่งแต่ละตัว
+        Dim MyToolTip As New ToolTip
+        With MyToolTip
+            .SetToolTip(Me.btnMoveRight, "ย้ายไอเทมที่เลือกไปทางขวา")
+            .SetToolTip(Me.btnMoveLeft, "ย้ายไอเทมที่เลือกไปทางซ้าย")
+            .SetToolTip(Me.btnMoveAllRight, "ย้ายไอเทมทั้งหมดไปทางขวา")
+            .SetToolTip(Me.btnMoveAllLeft, "ย้ายไอเทมทั้งหมดไปทางซ้าย")
+            .SetToolTip(Me.btnMoveUp, "ย้ายไอเทมที่เลือกขึ้นไปด้านบน")
+            .SetToolTip(Me.btnMoveDown, "ย้ายไอเทมที่เลือกลงไปด้านล่าง")
+        End With
+    End Sub
+
+    ' / ------------------------------------------------------------------------------
+    '// เลื่อน Item ขึ้น
+    Private Sub btnMoveUp_Click(sender As System.Object, e As System.EventArgs) Handles btnMoveUp.Click
+        '// Trap Error: หากไม่มีการเลือกไอเทม ก็ให้เด้งหนีออกไป
+        If ListBox2.SelectedItem = Nothing Then Exit Sub
+        '// การเลื่อนไอเทมขึ้น ไอเทมที่เลือกต้องมากกว่า 0 เพราะหาก SelectedIndex = 0 ก็คือแถวบนสุด จะออกจากเงื่อนไข
+        If ListBox2.SelectedIndex > 0 Then
+            '// การเลื่อนขึ้นไปข้างบนต้องทำการลดค่า SelectedIndex ลง 1 ค่า
+            Dim iRow As Integer = ListBox2.SelectedIndex - 1
+            '// แทรกไอเทมในแถวที่เลื่อนขึ้นไป
+            ListBox2.Items.Insert(iRow, ListBox2.SelectedItem)
+            '// ลบแถวตัวมันเองออกไป
+            ListBox2.Items.RemoveAt(ListBox2.SelectedIndex)
+            '// เลือกแถวปัจจุบันที่ถูกเลื่อนขึ้น
+            ListBox2.SelectedIndex = iRow
+        End If
+    End Sub
+
+    ' / ------------------------------------------------------------------------------
+    '// เลื่อน Item ลง
+    Private Sub btnMoveDown_Click(sender As System.Object, e As System.EventArgs) Handles btnMoveDown.Click
+        '// Trap Error: หากไม่มีการเลือกไอเทม ก็ให้เด้งหนีออกไป
+        If ListBox2.SelectedItem = Nothing Then Exit Sub
+        '// ไอเทมที่เลือกต้องน้อยกว่า จำนวนไอเทมทั้งหมดลบ 1 (Index นับที่ 0)
+        '// หากไอเทมที่เลือกมีค่าเท่ากับ ListBox2.Items.Count - 1 มันคือแถวลำดับสุดท้าย จะออกจากเงื่อนไข
+        If ListBox2.SelectedIndex < ListBox2.Items.Count - 1 Then
+            '// การเลื่อนแถวลง ต้องเลื่อนลงไปข้างล่าง 2 แถวก่อน เพราะการ Insert จะแทรกแถวขึ้นมาด้านบน 1 แถว
+            Dim iRow As Integer = ListBox2.SelectedIndex + 2
+            '// แทรกแถวไอเทมใหม่
+            ListBox2.Items.Insert(iRow, ListBox2.SelectedItem)
+            '// ลบแถวตัวมันเองออกไป
+            ListBox2.Items.RemoveAt(ListBox2.SelectedIndex)
+            '// ลบออก 1 เพื่อเลือกแถวที่เลื่อนลงไป
+            ListBox2.SelectedIndex = iRow - 1
+        End If
+    End Sub
+
+    ' / ------------------------------------------------------------------------------
+    ' / ย้ายไอเทมจาก ListBox1 ไป ListBox2
+    Private Sub btnMoveRight_Click(sender As System.Object, e As System.EventArgs) Handles btnMoveRight.Click
+        '// ยังไม่ได้เลือกไอเทม
+        If ListBox1.SelectedIndex < 0 Then
+            MessageBox.Show("กรุณาเลือกไอเทมในรายการที่ 1 ก่อน.", "รายงานสถานะ", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            '// เพิ่มไอเทมที่เลือกจาก ListBox1 ไปให้กับ ListBox2
+            'ListBox2.Items.Add(ListBox1.SelectedItem)
+            '// ลบไอเทมที่เลือกใน ListBox1 ออกไป
+            'ListBox1.Items.Remove(ListBox1.SelectedItem)
+
+            '// เก็บค่าไอเทมเอาไว้ใน Array แต่เป็นการจัดการเก็บสมาชิก (Collection) ในรูปแบบ List 
+            Dim mSelectItem = (From i In ListBox1.SelectedItems).ToList
+            '// วนรอบจากค่าไอเทมที่มีอยู่
+            For Each item In mSelectItem
+                '// เพิ่มไอเทมที่เลือกจาก ListBox1 ไปให้กับ ListBox2
+                ListBox2.Items.Add(ListBox1.SelectedItem)
+                '// ลบไอเทมที่เลือกใน ListBox1 ออกไป
+                ListBox1.Items.Remove(ListBox1.SelectedItem)
+            Next
+        End If
+    End Sub
+
+    ' / ------------------------------------------------------------------------------
+    ' / ย้ายไอเทมจาก ListBox2 ไป ListBox1
+    Private Sub btnMoveLeft_Click(sender As System.Object, e As System.EventArgs) Handles btnMoveLeft.Click
+        If ListBox2.SelectedIndex < 0 Then
+            MessageBox.Show("กรุณาเลือกไอเทมในรายการที่ 2 ก่อน.", "รายงานสถานะ", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            '// เพิ่มไอเทมที่เลือกจาก ListBox2 ไปให้กับ ListBox1
+            'ListBox1.Items.Add(ListBox2.SelectedItem)
+            '// ลบไอเทมที่เลือกใน ListBox2 ออกไป
+            'ListBox2.Items.Remove(ListBox2.SelectedItem)
+
+            '// เก็บค่าไอเทมเอาไว้ใน List 
+            Dim mSelectItem = (From i In ListBox2.SelectedItems).ToList
+            '// วนรอบจากค่าไอเทมที่มีอยู่
+            For Each item In mSelectItem
+                '// เพิ่มไอเทมที่เลือกจาก ListBox1 ไปให้กับ ListBox2
+                ListBox1.Items.Add(ListBox2.SelectedItem)
+                '// ลบไอเทมที่เลือกใน ListBox1 ออกไป
+                ListBox2.Items.Remove(ListBox2.SelectedItem)
+            Next
+        End If
+
+    End Sub
+
+    ' / ------------------------------------------------------------------------------
+    ' / ย้ายไอเทมทั้งหมดจาก ListBox1 ไป ListBox2
+    Private Sub btnMoveAllRight_Click(sender As System.Object, e As System.EventArgs) Handles btnMoveAllRight.Click
+        '// ลูปเอาไอเทมทั้งหมดที่มีอยู่ใน ListBox1 ไปให้กับ ListBox2
+        For Each item In ListBox1.Items
+            ListBox2.Items.Add(item)
+        Next
+        '// ลบไอเทมที่มีอยู่ทั้งหมดใน ListBox1 ออกไป
+        ListBox1.Items.Clear()
+    End Sub
+
+    ' / ------------------------------------------------------------------------------
+    ' / ย้ายไอเทมทั้งหมดจาก ListBox2 ไป ListBox1
+    Private Sub btnMoveAllLeft_Click(sender As System.Object, e As System.EventArgs) Handles btnMoveAllLeft.Click
+        '// ลูปเอาไอเทมทั้งหมดที่มีอยู่ใน ListBox2 ไปให้กับ ListBox1
+        For Each item In ListBox2.Items
+            ListBox1.Items.Add(item)
+        Next
+        '// ลบไอเทมที่มีอยู่ทั้งหมดใน ListBox2 ออกไป
+        ListBox2.Items.Clear()
+    End Sub
+
+End Class
